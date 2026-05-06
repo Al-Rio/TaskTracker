2b382af18ce92f830c9081bf5d35082175b29a21
Ryad Abdelgalil <Ryad.Abdelgalil@go.minneapolis.edu>
2026-05-06 14:59:04 +0000

    table divider commit

--- TaskTracker/obj/Debug/net10.0/TaskTracker.AssemblyInfo.cs
+++ TaskTracker/obj/Debug/net10.0/TaskTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e7cc3a55cb0fecb4fdf7e64379ca748bde51e76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee556414c664634416c1a61a56c48fb3e8ffaa97")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
